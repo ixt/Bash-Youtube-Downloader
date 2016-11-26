1903be9d9ca771630ce4a56a1c7e9eed92e3693b
NfN <orange@ff4500.red>
2016-11-26 16:32:16 +0000

    inital cleaning

--- get_video.sh
+++ get_video.sh
@@ -5,8 +5,6 @@
 #
 # usage: ./get_video.sh https://www.youtube.com/watch?v=xxxxxxxxxx
 #
-#
-#
 # Rev 1.0
 # 2013/09/03
 # Copyright 2013 Jacky Shih <iluaster@gmail.com>
@@ -54,39 +52,35 @@ echo "$1" > youtube_tmp.txt
 id_name=`perl -ne 'print "$1\n" if /v=(.*)/' youtube_tmp.txt`
 
   name="http://www.youtube.com/get_video_info?video_id=${id_name}"
+  wget "$name" -O tmp2.txt
 
-  wget "$name" -O "${id_name}_url.txt"
-
-#cut and filter mp4 url
-
+# cut and filter mp4 url
   cp "${id_name}_url.txt" tmp2.txt
   sed -e 's/&/\n/g' tmp2.txt| grep 'url_encoded_fmt_stream_map'> tmp3.txt
-  sed -i -e 's/%2C/,/g' tmp3.txt 
-  sed -i -e 's/,/\n/g' tmp3.txt
+  sed -i -e 's/%2C/,/g' -e 's/,/\n/g' tmp3.txt
 
-#print out total video format name and quality
+# print out total video format name and quality
   perl -ne 'print "$2,$1\n" if /quality%3D(.*?)%.*video%252F(.*?)(%|\n)/' tmp3.txt > video_type_option.txt
 
-#if video format name is prior to quality
+# if video format name is prior to quality
   perl -ne 'print "$1,$2\n" if /video%252F(.*?)%.*quality%3D(.*?)(%|\n)/' tmp3.txt >> video_type_option.txt
   sed -i -e 's/x-flv/flv/g' video_type_option.txt
 
   select_option
-  
-#set file extension name variable and video quality variable
+
+# set file extension name variable and video quality variable
   extension_name=`head -n "$n" video_type_option.txt | tail -n 1 | cut -d "," -f 1`
   quality_name=`head -n "$n" video_type_option.txt | tail -n 1 | cut -d "," -f 2`
 
-  sed -i -e 's/%26/\&/g' tmp4.txt
-  sed -i -e 's/&/\n/g' tmp4.txt
+  sed -i -e 's/%26/\&/g' -e 's/&/\n/g' tmp4.txt
   grep 'http' tmp4.txt > tmp5.txt
   grep 'sig%3D' tmp4.txt >> tmp5.txt
   perl -pe 's/\n//g' tmp5.txt | sed -e 's/sig%3D/\&signature%3D/g' > tmp6.txt
   sed -i -e 's/url%3D//g' tmp6.txt
-#url decoding
+# url decoding
   cat tmp6.txt | sed -e 's/%25/%/g' -e 's/%25/%/g' -e 's/%3A/:/g' -e 's/%2F/\//g' -e 's/%3F/\?/g' -e 's/%3D/=/g' -e 's/%26/\&/g' > tmp7.txt
 
   wget -i tmp7.txt -O "${id_name}_${quality_name}.${extension_name}"
-  
-  rm -f tmp[2-6].txt
+
+  rm -f tmp[2-7].txt
 
